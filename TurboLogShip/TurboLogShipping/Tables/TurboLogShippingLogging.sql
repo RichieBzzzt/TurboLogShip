8a9fa66bee6514c0d69878df7555514e04800798
Richie Lee <richard.lee@sabin.io>
2017-04-23 15:36:14 +0100

    delete table and inserts to table, becuase you can't write to a table in readonly mode now can you.....

--- TurboLogShip/TurboLogShipping/Tables/TurboLogShippingLogging.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [TurboLogShipping].[TurboLogShippingLogging]
-(
-	[Id] INT NOT NULL IDENTITY (1,1) PRIMARY KEY
-	,TimeOfMessage DATETIME2
-	,LogShippedDatabase SYSNAME
-	,RestoreJobName SYSNAME
-	,[Message] NVARCHAR (512)  
-)
